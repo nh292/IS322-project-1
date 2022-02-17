b6581c8b7b64d305218ad8afb123c4e9f5d9d8fb
nh292 <87101710+nh292@users.noreply.github.com>
2022-02-16 21:57:09 -0500

    test drop down menu

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var mockDatabase = [
-    { _id: '123', name: 'Article 1', published: true },
-    { _id: '583', name: 'Article 2', published: true },
+    { _id: '123', name: 'Article 1', published: false },
+    { _id: '583', name: 'Article 2', published: false },
     { _id: '954', name: 'Article 3', published: false },
-    { _id: '384', name: 'Article 4', published: false },
+    { _id: '384', name: 'Article 4', published: true },
     { _id: '183', name: 'Article 5', published: true },
-    { _id: '007', name: 'Article 6', published: false },
+    { _id: '007', name: 'Article 6', published: true },
     { _id: '304', name: 'Article 7', published: true },
-    { _id: '729', name: 'Article 8', published: false },
+    { _id: '729', name: 'Article 8', published: true },
     { _id: '734', name: 'Article 9', published: true, color: 'blue' },
 ];
 
